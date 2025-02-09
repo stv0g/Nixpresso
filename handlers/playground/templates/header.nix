bf4c3a4cfeb5b6a609be51cabbf3ac0178ebeefd
Steffen Vogel <post@steffenvogel.de>
2025-02-09 12:50:52 +0100

    fix(ui): Remove Bill Gates quote

--- handlers/playground/templates/header.nix
+++ handlers/playground/templates/header.nix
@@ -4,14 +4,6 @@
 { ... }:
 ''
   <section>
-      This is a playground for evaluating Nix expressions and building derivations. Results can be displayed, downloaded or shared.
-      <blockquote>
-          “The term, information at your fingertips, is to remind people what a broad role the personal computer will be playing.
-          It's not a computation device, it's not a word processing or a spreadsheet device.
-          It's a window onto the world of information.”
-          <footer style="text-align: right;">
-              <cite>— Bill Gates</cite>
-          </footer>
-      </blockquote>
+    <p>This is a playground for evaluating Nix expressions and building derivations. Results can be displayed, downloaded or shared.</p>
   </section>
 ''
