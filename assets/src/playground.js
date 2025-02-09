1a5a18fec9a521ba400e906046cb73e0f90d155e
Steffen Vogel <post@steffenvogel.de>
2025-02-09 13:04:49 +0100

    fix: Add missing file headers

--- assets/src/playground.js
+++ assets/src/playground.js
@@ -1,4 +1,5 @@
-
+// SPDX-FileCopyrightText: 2025 Steffen Vogel <post@steffenvogel.de>
+// SPDX-License-Identifier: Apache-2.0
 
 function showTooltip(elm, timeout, message) {
     elm.setAttribute("data-tooltip", message);
