e37e6859910d62971d1c4efb64fdd1928482d1c2
Steffen Vogel <post@steffenvogel.de>
2025-05-02 13:56:12 +0200

    feat(handlers): Add a web UI for the TTS handler
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

--- handlers/tts/default.nix
+++ handlers/tts/default.nix
@@ -10,8 +10,13 @@
   fetchurl,
 }:
 let
-  inherit (lib) head;
-  inherit (nixpresso.lib) mkHandler;
+  inherit (lib)
+    head
+    mapAttrsToList
+    groupBy
+    concatStringsSep
+    ;
+  inherit (nixpresso.lib) handlers url mkHandler;
 
   thorsten = {
     hessisch = {
@@ -27,39 +32,30 @@ let
     };
   };
 
-  fetchPiperVoice = callPackage ./fetch.nix { };
+  voices = callPackage ./voices.nix { };
 in
 mkHandler { description = "Synthesize speech from text using piper-tts"; } (
-  { query, ... }:
+  {
+    query,
+    path,
+    meta,
+    ...
+  }:
   let
-    fetchVoice =
-      args:
-      fetchPiperVoice (
-        args
-        // {
-          rev = "293cad0539066f86e6bce3b9780c472cc9157489";
-          hash = "sha256-HajmnzAvraAQRKVStbKZ+1ZOv/N3ptA3sAFAIoltJBQ=";
-        }
-      );
-
     queryDefault = key: default: head (query.${key} or [ default ]);
 
-    key = queryDefault "key" null;
+    key = queryDefault "key" "en_US-amy-medium";
     code = queryDefault "code" null;
     family = queryDefault "family" null;
     name = queryDefault "name" null;
     quality = queryDefault "quality" null;
+    text = queryDefault "text" "Hello world!";
 
     voice =
-      if key == null && code == null && family == null && name == null then
-        fetchVoice {
-          code = "en_US";
-          quality = "medium";
-        }
-      else if family != null && thorsten ? ${family} then
+      if family != null && thorsten ? ${family} then
         thorsten.${family}
       else
-        fetchVoice {
+        voices.fetch {
           inherit
             key
             code
@@ -69,20 +65,86 @@ mkHandler { description = "Synthesize speech from text using piper-tts"; } (
             ;
         };
 
-    defaultText = "Hello world!";
-    text = head (query.text or [ defaultText ]);
-  in
-  {
-    body = runCommandNoCC "tts.wav" { } ''
-      echo "${text}" | ${piper-tts}/bin/piper \
-        --quiet \
-        --output_file - \
-        --model ${voice.model} \
-        --config ${voice.config} > $out
-    '';
-
-    headers = {
-      "Content-Type" = "audio/wav";
+    audioQuery = url.encodeQueryString {
+      inherit
+        key
+        code
+        family
+        name
+        quality
+        text
+        ;
     };
-  }
+  in
+  if path == "/audio" then
+    {
+      body = runCommandNoCC "tts.wav" { } ''
+        echo "${text}" | ${piper-tts}/bin/piper \
+          --quiet \
+          --output_file - \
+          --model ${voice.model} \
+          --config ${voice.config} > $out
+      '';
+
+      headers = {
+        "Content-Type" = "audio/wav";
+      };
+    }
+  else
+    handlers.html {
+      title = meta.description;
+      main = ''
+        <p>
+          This example uses <a href="https://github.com/rhasspy/piper">piper TTS</a> to synthesize speech from text.
+        </p>
+
+        <section>
+          <h2>Input</h2>
+          <form>
+            <label>Text
+              <textarea class="editor" rows=10 name="text">${text}</textarea>
+            </label>
+            <label>Voice
+              <select name="key">
+                ${concatStringsSep "\n" (
+                  mapAttrsToList
+                    (language: voices: ''
+                      <optgroup label="${language}">
+                        ${concatStringsSep "\n" (
+                          map (
+                            voice:
+                            ''<option value="${voice.key}" ${
+                              if key == voice.key then "selected" else ""
+                            }>${voice.name} ${voice.quality}</option>''
+                          ) voices
+                        )}
+                      </optgroup>
+                    '')
+                    (
+                      groupBy (v: "${v.language.name_english} [${v.language.code}]") (
+                        mapAttrsToList (n: v: v) voices.voices
+                      )
+                    )
+                )}
+              </select>
+            </label>
+            <button type="submit" /><span class="mdi mdi-play" /> Text-to-Speech</button>
+          </form>
+        </section>
+
+        <section>
+          <h2>Output</h2>
+          <center>
+            <audio src="audio?${audioQuery}" autoplay controls style="margin-bottom: 3em;" />
+          </center>
+        </section>
+
+        <section>
+          <fieldset role="group">
+            <button onclick="window.location = '/';"><span class="mdi mdi-arrow-left" /> Back</button>
+            <button onclick="window.location = 'https://github.com/stv0g/Nixpresso/tree/main/handlers/tts';"><span class="mdi mdi-github""/> Code</button></a>
+          </fieldset>
+        </section>
+      '';
+    }
 )
