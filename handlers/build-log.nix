af49e9669eaaf5253af242eab259fc87e171a008
Steffen Vogel <post@steffenvogel.de>
2025-05-03 10:53:00 +0200

    feat: Add HTML frontends for build-log and derivation example handlers
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

--- handlers/build-log.nix
+++ handlers/build-log.nix
@@ -4,13 +4,54 @@
 { hello, nixpresso }:
 let
   inherit (builtins) pathExists;
-  inherit (nixpresso.lib) mkHandler;
+  inherit (nixpresso.lib) mkHandler handlers;
 in
-mkHandler { description = "Gets build log of a derivation"; } {
-  body = hello;
-  mode = "log";
+mkHandler { description = "Gets build log of a derivation"; } (
+  {
+    path,
+    meta,
+    basePath,
+    ...
+  }:
+  if path == "/log" then
+    {
+      body = hello;
+      mode = "log";
 
-  # Rebuild if it already exists in the store
-  # in order to streaming build output in real-time.
-  rebuild = pathExists hello.outPath;
-}
+      # Rebuild if it already exists in the store
+      # in order to streaming build output in real-time.
+      rebuild = pathExists hello.outPath;
+    }
+  else
+    handlers.html {
+      title = meta.description;
+      main = ''
+        <p>
+          This example gets the build log of a derivation (or builds it live) and uses <a href="https://xtermjs.org/">Xterm.js</a> to visualize the output.</br >
+          For the RAW output, please visit <a href="./log"><code>${basePath}/raw</code></a>.
+        </p>
+
+        <pre class="terminal" id="terminal"></pre>
+
+        <section>
+          <fieldset role="group">
+            <button onclick="window.location = '/';"><span class="mdi mdi-arrow-left" /> Back</button>
+            <button onclick="window.location = 'https://github.com/stv0g/Nixpresso/blob/main/handlers/build-log.nix';"><span class="mdi mdi-github"/> Code</button></a>
+          </fieldset>
+        <section>
+
+        <script type="module">
+          import { streamToTerminal } from '/assets/bundle.js';
+
+          document.addEventListener("DOMContentLoaded", async () => {
+            const url = new URL(window.location.href);
+            url.pathname += "log";
+            
+            const result = document.getElementById('terminal');
+
+            await streamToTerminal(result.terminal, url.href);
+          });
+        </script>
+      '';
+    }
+)
--- handlers/derivation.nix
+++ handlers/derivation.nix
@@ -1,20 +1,102 @@
 # SPDX-FileCopyrightText: 2025 Steffen Vogel <post@steffenvogel.de>
 # SPDX-License-Identifier: Apache-2.0
 
-{ hello, nixpresso }:
+{
+  pkgs,
+  lib,
+  nixpresso,
+}:
 let
-  inherit (nixpresso.lib) mkHandler;
+  inherit (builtins) head;
+  inherit (lib) optionalString;
+  inherit (nixpresso.lib)
+    mkHandler
+    handlers
+    ;
 in
 mkHandler
   {
-    description = "Show the derivation path of a Nixpkgs package in JSON format (append <tt>?recursive</tt> to include dependencies)";
+    description = "Get the derivation of a package in JSON format";
   }
   (
-    { query, ... }:
     {
-      body = hello;
-      mode = "derivation";
+      query,
+      path,
+      meta,
+      ...
+    }:
+    let
+      queryDefault = key: default: head (query.${key} or [ default ]);
+      packageName = queryDefault "package" "hello";
+      recursive = query ? "recursive";
 
-      recursive = query ? recursive;
-    }
+      package = pkgs.${packageName};
+    in
+    if path == "/raw" then
+      {
+        body = package;
+        mode = "derivation";
+
+        inherit recursive;
+      }
+    else
+      handlers.html {
+        title = meta.description;
+        main = ''
+          <p>
+            This example shows the derivation of a package in JSON format. The derivation is taken from Nixpkgs.
+            This is roughly equivalent to running <code>nix derivation show nixpkgs#${packageName}</code> on the CLI.
+            With the <emph>recursive</emph> option, the derivation's recursive output is returned, which corresponds to the <code>--recursive</code> parameter on the CLI.
+          </p>
+
+          <h2>Input</h2>
+
+          <form> 
+            <label for="input">
+              Package
+              <input type="text" id="input" name="package" value="${packageName}" />
+            </label>
+
+            <label for="recursive">
+              <input type="checkbox" id="recursive" name="recursive" value="true" ${optionalString recursive "checked"} />
+              Recursive
+            </label>
+
+            <input type="submit" value="Submit" style="margin-top: 2em;" />
+          </form>
+
+          <h2>Output</h2>
+
+          <pre id="editor" class="editor language-json"></pre>
+
+          <section>
+            <fieldset role="group">
+              <button onclick="window.location = '/';"><span class="mdi mdi-arrow-left" /> Back</button>
+              <button onclick="window.location = 'https://github.com/stv0g/Nixpresso/blob/main/handlers/derivation.nix';"><span class="mdi mdi-github"/> Code</button></a>
+            </fieldset>
+          <section>
+
+          <script type="module">
+            const form = document.getElementsByTagName('form')[0];
+
+            form.onsubmit = async (e) => {
+              e.preventDefault();
+
+              const formData = new FormData(form);
+              const params = new URLSearchParams(formData);
+
+              const url = new URL(window.location.href);
+              url.pathname += "raw";
+              url.search = params.toString();
+
+              const response = await fetch(url.href);
+              const text = await response.text();
+              const editor = document.getElementById('editor');
+              editor.setValue(text);
+            }
+
+            document.addEventListener("DOMContentLoaded", form.onsubmit);
+          </script>
+        '';
+      }
   )
